62a069d45b1963606322d81b134eed3fd385556e
samueltomen <tomennana.samuel@outlook.fr>
2024-12-19 15:20:00 +0100

    Ãminor fix

--- frontend/src/Context/AuthContext/AuthContext.jsx
+++ frontend/src/Context/AuthContext/AuthContext.jsx
@@ -3,13 +3,20 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    setIsLogged(!!token);
+    if (token) {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
   }, []);
 
+  if (isLogged === null) {
+    return null;
+  }
   return (
     <AuthContext.Provider value={{ isLogged, setIsLogged }}>
       {children}
